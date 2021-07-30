cb02a4579fd79f9ee31dfd20c0f3d0205bb9cb5e
mackliu <macklun@gmail.comm>
2021-07-30 11:09:58 +0800

    完成編輯電影功能

--- backend/movie.php
+++ backend/movie.php
@@ -42,7 +42,7 @@
                         <button class="show-movie" data-id="<?=$movie['id'];?>"><?=($movie['sh']==1)?'顯示':'隱藏';?></button>
                         <button id="<?=$up;?>" class='sw'>往上</button>
                         <button id="<?=$down;?>" class='sw'>往下</button>
-                        <button>編輯電影</button>
+                        <button onclick="location.href='?do=edit_movie&id=<?=$movie['id'];?>'">編輯電影</button>
                         <button class='del-btn' data-id="<?=$movie['id'];?>">刪除電影</button>
 
                     </div>
