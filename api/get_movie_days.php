4ec6b0d7bc06ce2bf2cefc27996e8b2ca78a13ad
mackliu <macklun@gmail.com>
2021-08-09 11:00:24 +0800

    修正上映日期天數計算問題

--- api/get_movie_days.php
+++ api/get_movie_days.php
@@ -3,9 +3,12 @@
 $movie=$Movie->find($_GET['movie']);
 $ondate=strtotime($movie['ondate']);
 $today=strtotime(date("Y-m-d"));
-$days=($today-$ondate)/(60*60*24);
+$days=($today-$ondate)/(60*60*24);  //距離上映日期的天數
 
-for($i=0;$i<=$days;$i++){
+//3-$days 可上映的日期
+
+
+for($i=0;$i<(3-$days);$i++){
     $date=date("Y-m-d",strtotime("+$i days",$today));
     $showDate=date("m-d l",strtotime("+$i days",$today));
     echo "<option value='$date'>";
